dc7954b82bed15730ee774c2f636d1c55fa0c534
Harsh Mishra <mishra.harshmishra727@gmail.com>
2022-03-15 19:33:59 +0000

    changes made forom theia lab enbironment

--- firstpython.py
+++ firstpython.py
@@ -1,2 +1,4 @@
 #new python file
 print("Hello there! Genereal Kenobi.")
+print ("this is done from some lab in course")
+print("only first line was written in git hum now this plaform mimics a local device")
